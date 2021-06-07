57324cf5eee4f0f6a148bc96155c5dc5548c9cac
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2021-06-07 14:30:05 +0900

    Update config.js

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -4,6 +4,13 @@ module.exports = {
   dest: "build",
   base: "js-tutorial",
   themeConfig: {
-    sidebar: ["introduce", "1.whatIsJS", "components"]
+    sidebar: ["introduce", "1.whatIsJS", "components"],
+    nav: [
+      {
+        text: "GitHub", link: "https://github.com/republic-of-JS"
+      },{
+        text: "Discord", link: "https://discord.gg/68dBHx56rm"
+      }
+    ]
   }
 }
