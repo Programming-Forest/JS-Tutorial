4f16543efb74a3335d2dbedcfef1b8d9e4702bc0
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2021-06-07 13:35:25 +0900

    Create config.js

---
+++ documents/.vuepress/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  title: "JS-Tutorial",
+  description: "about JavaScript",
+  dest: "build",
+  base: "js-tutorial",
+  themeConfig: {
+    sidebar: ["introduce", "1.whatIsJS", "components"]
+  }
+}
