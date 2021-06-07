ea94bd6be92ab3f331312465a6c652c083cd8406
오병진 <64676070+sunrabbit123@users.noreply.github.com>
2021-06-07 14:34:09 +0900

    Update config.js

--- documents/.vuepress/config.js
+++ documents/.vuepress/config.js
@@ -2,7 +2,7 @@ module.exports = {
   title: "JS-Tutorial",
   description: "about JavaScript",
   dest: "build",
-  base: "js-tutorial",
+  base: "/js-tutorial/",
   themeConfig: {
     sidebar: ["introduce", "1.whatIsJS", "components"],
     nav: [
