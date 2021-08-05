f92a86e115964935a50200e30cfb99b994806b13
sunrabbit123 <qudwls185@naver.com>
2021-08-05 23:58:35 +0900

    update; 파일 구조

--- path.js
+++ path.js
@@ -1,1 +1,19 @@
-module.exports = ["introduce", "components"];
+const IntroduceList = ["introduce", "components"];
+const BasicsList = [];
+module.exports = [
+  {
+    title: "시작하기에 앞서..",
+    path: "",
+    sidebarDepth: 2,
+  },
+  {
+    title: "언어 소개 및 환경 구축",
+    children: IntroduceList.map((f) => `Introduce/${f}`),
+    sidebarDepth: 2,
+  },
+  {
+    title: "기본 문법 및 자료형",
+    children: BasicsList.map((f) => `Basics/${f}`),
+    sidebarDepth: 2,
+  },
+];
